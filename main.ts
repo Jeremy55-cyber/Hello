9e3e7dd82c045abcd0b33909a888f5216d11b4b8
Jeremy55-cyber <73191152+Jeremy55-cyber@users.noreply.github.com>
2020-10-20 14:41:22 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
-input.onGesture(Gesture.Shake, function () {
-    basic.showIcon(IconNames.Heart)
-    basic.pause(500)
+input.onButtonPressed(Button.A, function () {
+    if (échange == 0) {
+        nombre1 += 1
+        basic.showNumber(nombre1)
+    }
+    if (échange == 1) {
+        nombre2 += 1
+        basic.showNumber(nombre2)
+    }
+})
+input.onButtonPressed(Button.B, function () {
     basic.clearScreen()
+    échange += 1
+    if (échange == 2) {
+        basic.showNumber(nombre1 + nombre2)
+    }
 })
-basic.forever(function () {
-	
-})
+let échange = 0
+let nombre2 = 0
+let nombre1 = 0
+nombre1 = 0
+nombre2 = 0
+échange = 0
