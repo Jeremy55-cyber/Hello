ede93e51a8755202cb5c58d9a71efa5247794db9
Jeremy55-cyber <73191152+Jeremy55-cyber@users.noreply.github.com>
2020-10-27 14:39:41 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,16 +8,91 @@ input.onButtonPressed(Button.A, function () {
         basic.showNumber(nombre2)
     }
 })
+input.onButtonPressed(Button.AB, function () {
+    op += 1
+    if (op == 1) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # # # # #
+            . . # . .
+            . . # . .
+            `)
+        basic.pause(100)
+        basic.clearScreen()
+    } else {
+        if (op == 2) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                # # # # #
+                . . . . .
+                . . . . .
+                `)
+            basic.pause(100)
+            basic.clearScreen()
+        } else {
+            if (op == 3) {
+                basic.showLeds(`
+                    # . . . #
+                    . # . # .
+                    . . # . .
+                    . # . # .
+                    # . . . #
+                    `)
+                basic.pause(100)
+                basic.clearScreen()
+            } else {
+                if (op == 4) {
+                    basic.showLeds(`
+                        . . # . .
+                        . . . . .
+                        # # # # #
+                        . . . . .
+                        . . # . .
+                        `)
+                    basic.pause(100)
+                    basic.clearScreen()
+                } else {
+                    op = 1
+                    basic.showLeds(`
+                        . . # . .
+                        . . # . .
+                        # # # # #
+                        . . # . .
+                        . . # . .
+                        `)
+                    basic.pause(100)
+                    basic.clearScreen()
+                }
+            }
+        }
+    }
+})
 input.onButtonPressed(Button.B, function () {
     basic.clearScreen()
     échange += 1
     if (échange == 2) {
-        basic.showNumber(nombre1 + nombre2)
+        if (op == 1) {
+            basic.showNumber(nombre1 + nombre2)
+        } else {
+            if (op == 2) {
+                basic.showNumber(nombre1 - nombre2)
+            } else {
+                if (op == 3) {
+                    basic.showNumber(nombre1 * nombre2)
+                } else {
+                    basic.showNumber(nombre1 / nombre2)
+                }
+            }
+        }
     }
 })
 let échange = 0
 let nombre2 = 0
 let nombre1 = 0
+let op = 0
+op = 1
 nombre1 = 0
 nombre2 = 0
 échange = 0
